28f149f4e7687655c35d94bed5e66123002e54ae
Richard Veitch <richardveitch77@gmail.com>
2015-07-04 17:45:06 -0700

    Changes used for fbaseball

--- examples/MockResultSet.java
+++ examples/MockResultSet.java
@@ -1169,6 +1169,18 @@ public class MockResultSet implements ResultSet
 	    // TODO Auto-generated method stub
 	    return null;
     }
+
+	@Override
+	public <T> T getObject(int arg0, Class<T> arg1) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> T getObject(String arg0, Class<T> arg1) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
 
 
